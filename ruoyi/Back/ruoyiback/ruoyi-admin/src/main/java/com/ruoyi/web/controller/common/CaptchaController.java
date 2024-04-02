c2718baa48cb82206f33a2e790af43a0b731c13a
herbiekang <herbiekang@gmail.com>
2024-04-02 18:04:35 +0800

    first;captcha is ok.

--- ruoyi/Back/ruoyiback/ruoyi-admin/src/main/java/com/ruoyi/web/controller/common/CaptchaController.java
+++ ruoyi/Back/ruoyiback/ruoyi-admin/src/main/java/com/ruoyi/web/controller/common/CaptchaController.java
@@ -40,10 +40,53 @@ public class CaptchaController
     @Autowired
     private RedisCache redisCache;
 
-//    @Autowired
-//    private ISysConfigService configService;
+    @Autowired
+    private ISysConfigService configService;
     /**
      * 生成验证码
      */
+
+    @GetMapping("/captchaImage")
+    public AjaxResult getCode(HttpServletResponse response) throws IOException{
+        AjaxResult ajax = AjaxResult.success();
+        boolean captchaEnabled = configService.selectCaptchaEnabled();
+        ajax.put("captchaEnabled", captchaEnabled);
+        if(!captchaEnabled){
+            return ajax;
+        }
+        String uuid = IdUtils.simpleUUID();
+        String verifyKey = CacheConstants.CAPTCHA_CODE_KEY + uuid;
+        String capStr = null;
+        String code = null;
+        BufferedImage image = null;
+
+        String captchaType = RuoYiConfig.getCaptchaType();
+        if ("math".equals(captchaType)){
+            String capText = captchaProducerMath.createText();
+            capStr = capText.substring(0, capText.lastIndexOf("@"));
+            code = capText.substring(capText.lastIndexOf("@") + 1);
+            image = captchaProducerMath.createImage(capStr);
+
+        }else if ("char".equals(captchaType)){
+            capStr = code = captchaProducer.createText();
+            image = captchaProducer.createImage(capStr);
+        }
+
+        redisCache.setCacheObject(verifyKey,code, Constants.CAPTCHA_EXPIRATION, TimeUnit.MINUTES);
+
+        FastByteArrayOutputStream os = new FastByteArrayOutputStream();
+        try
+        {
+            ImageIO.write(image, "jpg", os);
+        }
+        catch (IOException e)
+        {
+            return AjaxResult.error(e.getMessage());
+        }
+
+        ajax.put("uuid", uuid);
+        ajax.put("img", Base64.encode(os.toByteArray()));
+        return ajax;
+    }
 
 }
