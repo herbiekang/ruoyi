294e8cf2a663c8cce1857444c71eb76126fc6360
herbiekang <herbiekang@gmail.com>
2024-04-02 12:38:02 +0800

    base

--- ruoyi/Back/ruoyiback/ruoyi-admin/src/main/java/com/ruoyi/web/controller/common/CaptchaController.java
+++ ruoyi/Back/ruoyiback/ruoyi-admin/src/main/java/com/ruoyi/web/controller/common/CaptchaController.java
@@ -1,18 +1,49 @@
 package com.ruoyi.web.controller.common;
 
-import com.google.code.kaptcha.Producer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 import javax.annotation.Resource;
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.FastByteArrayOutputStream;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.google.code.kaptcha.Producer;
+import com.ruoyi.common.config.RuoYiConfig;
+import com.ruoyi.common.constant.CacheConstants;
+import com.ruoyi.common.constant.Constants;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.core.redis.RedisCache;
+import com.ruoyi.common.utils.sign.Base64;
+import com.ruoyi.common.utils.uuid.IdUtils;
+import com.ruoyi.system.service.ISysConfigService;
+import com.ruoyi.framework.config.CaptchaConfig;
 
+
+/**
+ * 验证码操作处理
+ *
+ * @author ruoyi
+ */
 @RestController
-public class CaptchaController {
-    /*@Resource(name = "captchaProducer")
+public class CaptchaController
+{
+    @Resource(name = "captchaProducer")
     private Producer captchaProducer;
+
     @Resource(name = "captchaProducerMath")
     private Producer captchaProducerMath;
 
     @Autowired
-    private RedisCache redisCache;*/
+    private RedisCache redisCache;
+
+//    @Autowired
+//    private ISysConfigService configService;
+    /**
+     * 生成验证码
+     */
+
 }
