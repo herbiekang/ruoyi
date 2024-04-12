dcbd2fae6371ae9cc144a937f23bbfd3c23c0d31
herbiekang <herbiekang@gmail.com>
2024-04-12 15:35:20 +0800

    front menu sidebar is ok

--- ruoyi/front/src/main.js
+++ ruoyi/front/src/main.js
@@ -3,10 +3,14 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 import ElementUI from 'element-ui'
+import './assets/styles/element-variables.scss'
+import '@/assets/styles/index.scss' // global css
+import '@/assets/styles/ruoyi.scss' // ruoyi css
 import './assets/icons' // icon
 import 'element-ui/lib/theme-chalk/index.css'
 import test1 from './views/test1.vue'
 import './permission';
+
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 
